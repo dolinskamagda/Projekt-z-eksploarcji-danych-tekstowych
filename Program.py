d0391f602998bdbcb5fba09a1af885ea487d216f
Magdalena <magdalena.dolinska2@gmail.com>
2024-06-02 15:15:56 +0200

    Zaaktualizowano pliki.

---
+++ Program.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly as py
+import plotly.graph_objs as go
+import cufflinks as cf
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
--- Untitled-1.py
+++
@@ -1,2 +0,0 @@
-git config --global user.name "Magdalena"
-git config --global user.email "magdalena.dolinska2@gmail.com"
