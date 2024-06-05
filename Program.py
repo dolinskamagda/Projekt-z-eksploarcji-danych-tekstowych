7b979aadf2f0c6ca891a482ea45f498490d6221b
Magdalena <magdalena.dolinska2@gmail.com>
2024-06-05 19:01:22 +0200

    Zakonczono stemming i lemantyzację.

--- Program.py
+++ Program.py
@@ -9,29 +9,33 @@ from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
 base_file = pd.read_csv(r"C:\Users\06mid\OneDrive\Pulpit\projekt eksploracja danych\Musical_instruments_reviews.csv")
 
+base_file['reviewText'] = base_file['reviewText'].astype('string')
 
 #Wstępna analiza danych
 
 base_file.info()
-print(base_file.head())
+# print(base_file.head())
+
 
 base_file = base_file.dropna()
 
-base_file['reviewText'] = base_file['reviewText'].astype('string')
-
 
 base_file['word_count'] = base_file['reviewText'].apply(lambda x: len(str(x).split(" ")))
-print(base_file[['reviewText','word_count']].head())
-print(f'Mean: {base_file.word_count.mean()}')
+# print(base_file[['reviewText','word_count']].head())
+# print(f'Mean: {base_file.word_count.mean()}')
 
 base_file['char_count'] = base_file['summary'].str.len()
-print(base_file[['summary','char_count']].head())
-print(f'Mean: {base_file.char_count.mean()}')
+# print(base_file[['summary','char_count']].head())
+# print(f'Mean: {base_file.char_count.mean()}')
 
 base_file['sentence'] = base_file['reviewText'].apply(lambda x: len([x for x in x.split() if x.endswith('.')]))
-print(base_file[['reviewText','sentence']].iloc[0:].head(20))
-print(f'Mean: {base_file.sentence.mean()}')
-#
+# print(base_file[['reviewText','sentence']].iloc[0:].head(20))
+# print(f'Mean: {base_file.sentence.mean()}')
+
+base_file['exclam'] = base_file['reviewText'].apply(lambda x: len([x for x in x.split() if x.endswith('!')]))
+# print(base_file[['reviewText','exclam']].iloc[0:].head(20))
+# print(f'Mean: {base_file.exclam.mean()}')
+
 import nltk
 from nltk.corpus import stopwords
 nltk.download('stopwords')
@@ -40,41 +44,11 @@ stop = stopwords.words('english')
 # # print(stop[:10])
 #
 base_file['stopwords'] = base_file['reviewText'].apply(lambda x: len([x for x in x.split() if x in stop]))
-print(base_file[['reviewText','stopwords']].head())
-print(f'Mean: {base_file.stopwords.mean()}')
-
-base_file['exclam'] = base_file['reviewText'].apply(lambda x: len([x for x in x.split() if x.endswith('!')]))
-print(base_file[['reviewText','exclam']].iloc[0:].head(20))
-print(f'Mean: {base_file.exclam.mean()}')
-
-# from textblob import TextBlob
-#
-# # Słownik mapujący tagi części mowy z TextBlob na kategorie
-# pos_family = {
-#     'noun': ['NN', 'NNS', 'NNP', 'NNPS'],
-#     'pron': ['PRP', 'PRP$', 'WP', 'WP$'],
-#     'verb': ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'],
-#     'adj': ['JJ', 'JJR', 'JJS'],
-#     'adv': ['RB', 'RBR', 'RBS', 'WRB']
-# }
-#
-# # Funkcja do zliczania określonej części mowy w tekście
-# def count_pos(text1, part_of_speech):
-#     count = 0
-#     if text1:
-#         blob = TextBlob(str(text1))
-#         count = sum(1 for word, tag in blob.tags if tag in pos_family[part_of_speech])
-#     return count
-# #
-# # # Zastosowanie funkcji do DataFrame
-# for part in pos_family:
-#     base_file[f'{part}_count'] = base_file['reviewText'].apply(lambda x: count_pos(x, part))
-# #
-# # # Wydrukuj wyniki
-# print(base_file.head(20)[['reviewText'] + [f'{part}_count' for part in pos_family]].dropna())
+# print(base_file[['reviewText','stopwords']].head())
+# print(f'Mean: {base_file.stopwords.mean()}')
 
 review_counts = base_file['reviewerName'].value_counts()
-print(review_counts)
+# print(review_counts)
 
 bins = [1, 2, 3, 4, 5, 6]
 base_file['overall'].hist(bins=bins, width=0.95, edgecolor='black', align='left')
@@ -82,9 +56,9 @@ plt.title('Distribution of Ratings')
 plt.xlabel('Rating')
 plt.ylabel('Frequency')
 plt.xticks([1, 2, 3, 4, 5])
-plt.show()
-
-
+# plt.show()
+#
+#
 def transform_helpful(row):
     # Usunięcie nawiasów kwadratowych i podział stringa na dwa elementy
     positive, total = map(int, row.strip('[]').split(', '))
@@ -94,37 +68,61 @@ def transform_helpful(row):
 base_file[['helpful_yes', 'helpful_total']] = base_file['helpful'].apply(transform_helpful)
 #
 # Wyświetlenie podstawowych statystyk
-print(base_file[['helpful_yes', 'helpful_total']].describe())
+# print(base_file[['helpful_yes', 'helpful_total']].describe())
 #
 # Obliczenie stosunku pozytywnych reakcji do całkowitej liczby reakcji
 base_file['helpful_ratio'] = base_file['helpful_yes'] / base_file['helpful_total']
-print(base_file[['reviewText', 'helpful_ratio']])
-
+# print(base_file[['reviewText', 'helpful_ratio']])
+#
 base_file['helpful_ratio'] *= 100
 
 base_file['helpful_ratio'].hist(bins=10, edgecolor='black')
 plt.title('Distribution of Helpfulness Ratio')
 plt.xlabel('Helpfulness Ratio')
 plt.ylabel('Frequency')
-plt.show()
-
-#Normalizacja tekstu
-base_file['reviewText'] = base_file['reviewText'].apply(lambda x: " ".join(x.lower() for x in x.split()))
-print(base_file['reviewText'].head())
+# plt.show()
 #
+# #Normalizacja tekstu
+base_file['reviewText'] = base_file['reviewText'].apply(lambda x: " ".join(x.lower() for x in x.split()))
+# print(base_file['reviewText'].head())
+# #
+# Usuwanie nadmiarowych spacji bezpośrednio w DataFrame
+base_file['reviewText'] = base_file['reviewText'].apply(lambda x: " ".join(x.split()))
+
+# print(base_file['reviewText'].head(10))
+
 stop = stopwords.words('english')
 base_file['reviewText'] = base_file['reviewText'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
-print(base_file['reviewText'].head())
-
+# print(base_file['reviewText'].head())
+#
 freq = pd.Series(' '.join(base_file['reviewText']).split()).value_counts()
 freq = freq[freq > 500]
-print(freq[:10])
-#
+# print(freq[:10])
+# #
 
-from nltk.stem import PorterStemmer
-ps = PorterStemmer()
-print(base_file['reviewText'][:5].apply(lambda x: " ".join([ps.stem(word) for word in x.split()])))
+# from nltk.stem import PorterStemmer
+# from nltk.tokenize import word_tokenize
+# import spacy
 
+# Pobranie niezbędnych zasobów
+nltk.download('punkt')
 
+# stemmer = PorterStemmer()
+# nlp = spacy.load('en_core_web_sm')
+
+def stem_text(text):
+    words = word_tokenize(text)
+    return ' '.join(stemmer.stem(word) for word in words)
+
+def lemmatize_text(text):
+    doc = nlp(text)
+    return ' '.join(token.lemma_ for token in doc)
+
+# base_file['StemmedText'] = base_file['reviewText'].apply(stem_text)
+# base_file['LemmatizedText'] = base_file['reviewText'].apply(lemmatize_text)
+
+# Wyświetlenie pierwszych kilku wierszy dla podglądu przetworzonych danych
+# print(base_file[['reviewText', 'StemmedText']].head())
+# print(base_file[['reviewText', 'LemmatizedText']].head())
 
 
